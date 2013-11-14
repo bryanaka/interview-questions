96d7e6e1d271399460df1dce7bcabbc27ae29db3
Bryan M. Robles <bryanaka0@gmail.com>
2013-11-13 16:54:06 -0800

    added some tests for minesweeper square class

--- ruby/minesweeper/square.rb
+++ ruby/minesweeper/square.rb
@@ -1,2 +1,7 @@
 class Square
+	attr_accessor :x, :y, :contents
+
+	def initialize(x, y, contents = nil)
+		@x, @y, @contents = x, y, contents
+	end
 end
--- ruby/minesweeper/square_spec.rb
+++ ruby/minesweeper/square_spec.rb
@@ -2,10 +2,32 @@ require 'rspec'
 require File.expand_path '../square', __FILE__
 
 describe Square do
-	let(:square) { Square.new(1,1,nil) }
+	let(:square) { Square.new(1,1,0) }
 
 	it 'has cordinates X and Y' do
 		expect(square.x).to eq(1)
 		expect(square.y).to eq(1)
+	end
+
+	describe '#mark!' do
+		it 'returns its contents' do
+			expect(square.mark!).to eq(0)
+		end
+
+		it 'can only happen once' do
+			square.mark!
+			expect(square.mark!).to raise_exception
+		end
+	end
+	
+	describe '#to_s changes when marked' do
+		xit 'and is empty' do
+		end
+
+		xit 'and has has a number' do
+		end
+	end
+
+	xit 'can be flagged' do
 	end
 end
